3a303537a851f899854e212a32b4f8cdb0797f16
maufaka <maufaka47@gmail.com>
2019-10-02 17:36:08 +0300

    Задания 4, 5, 6

--- script/main.js
+++ script/main.js
@@ -27,3 +27,97 @@ if (a >= 0 && b >= 0) {
 }
 // Задание 4:
 console.log('Задание 4:');
+a = +prompt('Введите число от 0 до 15');
+switch (a) {
+    case 0:
+        console.log('1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 1:
+        console.log('2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 2:
+        console.log('3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 3:
+        console.log('4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 4:
+        console.log('5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 5:
+        console.log('6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 6:
+        console.log('7, 8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 7:
+        console.log('8, 9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 8:
+        console.log('9, 10, 11, 12, 13, 14, 15');
+    break;
+    case 9:
+        console.log('10, 11, 12, 13, 14, 15');
+    break;
+    case 10:
+        console.log('11, 12, 13, 14, 15');
+    break;
+    case 11:
+        console.log('12, 13, 14, 15');
+    break;
+    case 12:
+        console.log('13, 14, 15');
+    break;
+    case 13:
+        console.log('14, 15');
+    break;
+    case 14:
+        console.log('15');
+    break;
+    case 15:
+        console.log('Вы ввели 15.');
+    break;
+    default:
+        console.log('Число должно быть от 0 до 15!');
+}
+// Задание 5:
+console.log ('Задание 5-6');
+function mathAddition (x, y) {
+    let z = x + y;
+    return 'Сумма ' + x  + ' + ' + y + ' равна ' + z;
+}
+
+function mathSubtraction (x, y) {
+    let z = x - y;
+    return 'Разность ' + x  + ' + ' + y + ' равна ' + z;
+}
+
+function mathMultiplication (x, y) {
+    let z = x * y;
+    return 'Произведение ' + x  + ' + ' + y + ' равно ' + z;
+}
+
+function mathDivision (x, y) {
+    let z = x / y;
+    return 'Деление ' + x  + ' + ' + y + ' равно ' + z;
+}
+// Задание 6:
+function mathOperation(arg1, arg2, operation) {
+    var Addition = operation;
+    switch (operation) {
+        case sum:
+            mathAddition (arg1, arg2);
+        break;
+        case subtraction:
+            mathSubtraction (arg1, arg2);
+        break;
+        case multiplication:
+            mathMultiplication (arg1, arg2);
+        break;
+        case divison:
+            mathDivision (arg1, arg2);
+        break;
+        default:
+            console.log('Введите корректную операцию!');
+    }
+}
