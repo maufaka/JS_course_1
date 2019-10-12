172f2bad9199a5504065139f70d198982ddf65bc
maufaka <maufaka47@gmail.com>
2019-10-12 18:50:42 +0300

    hw5

--- script/main.js
+++ script/main.js
@@ -1,125 +1,56 @@
-// Задание 1:
-var a = 1, b = 1, c, d;
-c = ++a;
-d = b++;
-c = (2+ ++a); 
-d = (2+ b++);
-console.log('Задание 1:');
-console.log('Код дает такие разультаты из-за постфиксной и префиксной записи а++ и ++а');
-// Задание 2:
-a = 2;
-var x = 1 + (a *= 2);
-console.log('Задание 2:');
-console.log(x);
-// Задание 3:
-console.log('Задание 3:');
-a = +prompt('Введите целое число:');
-b = +prompt('Введите целое число:');
-if (a >= 0 && b >= 0) {
-    c = a - b;
-    console.log('Разность введенных чисел равна: ' + c);
-} else if (a < 0 && b < 0) {
-    c = a * b;
-    console.log('Произведение введенных чисел равно: ' + c);
-} else {
-    c = a + b;
-    console.log('Сумма введенных чисел равно: ' + c);
-}
-// Задание 4:
-console.log('Задание 4:');
-a = +prompt('Введите число от 0 до 15');
-switch (a) {
-    case 0:
-        console.log('1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 1:
-        console.log('2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 2:
-        console.log('3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 3:
-        console.log('4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 4:
-        console.log('5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 5:
-        console.log('6, 7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 6:
-        console.log('7, 8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 7:
-        console.log('8, 9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 8:
-        console.log('9, 10, 11, 12, 13, 14, 15');
-    break;
-    case 9:
-        console.log('10, 11, 12, 13, 14, 15');
-    break;
-    case 10:
-        console.log('11, 12, 13, 14, 15');
-    break;
-    case 11:
-        console.log('12, 13, 14, 15');
-    break;
-    case 12:
-        console.log('13, 14, 15');
-    break;
-    case 13:
-        console.log('14, 15');
-    break;
-    case 14:
-        console.log('15');
-    break;
-    case 15:
-        console.log('Вы ввели 15.');
-    break;
-    default:
-        console.log('Число должно быть от 0 до 15!');
-}
-// Задание 5:
-console.log ('Задание 5-6');
-function mathAddition (x, y) {
-    let z = x + y;
-    return 'Сумма ' + x  + ' + ' + y + ' равна ' + z;
-}
-
-function mathSubtraction (x, y) {
-    let z = x - y;
-    return 'Разность ' + x  + ' и ' + y + ' равна ' + z;
-}
-
-function mathMultiplication (x, y) {
-    let z = x * y;
-    return 'Произведение ' + x  + ' и ' + y + ' равно ' + z;
-}
-
-function mathDivision (x, y) {
-    let z = x / y;
-    return 'Деление ' + x  + ' на ' + y + ' равно ' + z;
-}
-console.log(mathDivision (10, -2));
-// Задание 6:
-function mathOperation(arg1, arg2, operation) {
-    switch (operation) {
-        case 'addition':
-            console.log(mathAddition (arg1, arg2));
-        break;
-        case 'subtraction':
-            console.log(mathSubtraction (arg1, arg2));
-        break;
-        case 'multiplication':
-            console.log(mathMultiplication (arg1, arg2));
-        break;
-        case 'divison':
-            console.log(mathDivision (arg1, arg2));
-        break;
-        default:
-            console.log('Введите корректную операцию!');
-    }
-}
-
-console.log(mathOperation(3, 5, 'multiplication'));
+// // Задание 1:
+// var max = 999;
+// var x = +prompt('Введите число от 0 до 999');
+// var digit = {
+//     units: 0,
+//     tens: 0,
+//     hundreds: 0,
+// };
+// function search () {
+//     if (x > 0 && x <= 9){
+//         digit.units = x;
+//     } else if (x > 0 && x <= 999){
+//         digit.units = Math.floor(x % 10);
+//         digit.tens = Math.floor(x / 10 % 10);
+//         digit.hundreds = Math.floor(x / 100 % 10);
+//     }else {
+//         console.log('Вы ввели число за диапазоном 0 - 999');
+//     }
+//     console.log(digit);
+// }
+// // Задание 2:
+// const ITEMS = ['Samsung', 'Honor', 'Iphone', 'ZTE', 'Xiaomi', 'Sony', 'Lenovo'];
+// const PRICES = [400, 350, 500, 300, 320, 370, 270];
+// const ID = [1, 2, 3, 4, 5, 6, 7];
+// let prod = createCat ();
+// function createCat () {
+//     let arr = [];
+//     for (let i = 0; i < ITEMS.length; i++) {
+//         arr.push (createProd(ITEMS[i], PRICES[i], ID[i]))
+//     }
+//     return arr;
+// }
+// function createProd (name, price, id) {
+//     return {
+//         name: name,
+//         price: price,
+//         id: id
+//     }
+// }
+// let store = {
+//     catlog: [],
+//     cart: [],
+//     create: function () {
+//         for (let i = 0; i < ITEMS.length; i++) {
+//             this.catlog.push (this._createProd(ITEMS[i], PRICES[i], ID[i]));
+//         }
+//     },
+//     _createProd: function (name, price, id) {
+//         return {
+//             name: name,
+//             price: price,
+//             id: id
+//         }
+//     }
+// }
+// // Камень-ножницы-бумага
