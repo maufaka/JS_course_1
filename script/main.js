76761c6b830a247ef7f26527cc7d949b6019ce4a
maufaka <maufaka47@gmail.com>
2019-10-24 21:44:39 +0300

    Cart

--- script/main.js
+++ script/main.js
@@ -1,8 +1,18 @@
+const image = 'http://placehold.it/300x200';
+const imageCert = 'http://placehold.it/100x80';
 
 const ITEMS = ['Samsung', 'Honor', 'Iphone', 'ZTE', 'Xiaomi', 'Sony', 'Lenovo'];
 const PRICES = [400, 350, 500, 300, 320, 370, 270];
 const ID = [1, 2, 3, 4, 5, 6, 7];
 let prod = createCat ();
+
+let btnCart = document.querySelector ('.btn-cart');
+btnCart.addEventListener ('click', showCart);
+
+function showCart () {
+    document.querySelector ('.cart-block').classList.toggle ('ivisible');
+}
+
 function createCat () {
     let arr = [];
     for (let i = 0; i < ITEMS.length; i++) {
@@ -15,7 +25,7 @@ function createProd (name, price, id) {
         name: name,
         price: price,
         id: id,
-        img: Image,
+        img: image,
         createTemplate: function () {
             return `
             <div class="product-item">
@@ -30,6 +40,17 @@ function createProd (name, price, id) {
         }
     }
 }
+
+function renderCatalog () {
+    let htmlStr = '';
+    prod.forEach (el => {
+        htmlStr += el.createTemplate ();
+    })
+    document.querySelector ('.products').innerHTML = htmlStr;
+}
+
+renderCatalog ();
+
 let store = {
     catlog: [],
     cart: [],
